8041c6170990c8c4addb3363d8adb3558299fe97
DinisCruz <dinis.cruz@owasp.org>
2012-03-13 01:10:49 +0000

    adding code for 'O2_ImportExport_OunceLabs'

---
+++ O2_ImportExport_OunceLabs/DI.cs
@@ -0,0 +1,23 @@
+// This file is part of the OWASP O2 Platform (http://www.owasp.org/index.php/OWASP_O2_Platform) and is released under the Apache 2.0 License (http://www.apache.org/licenses/LICENSE-2.0)
+using O2.Interfaces.O2Core;
+using O2.Kernel;
+
+namespace O2.ImportExport.OunceLabs
+{
+    class DI
+    {
+        static DI()
+        {
+            log = PublicDI.log;
+            reflection = PublicDI.reflection;  // new O2FormsReflectionASCX();
+            config = PublicDI.config;
+        }
+
+        // DI Targets
+        //public static IReflectionASCX reflection { get; set; }
+        public static IReflection reflection { get; set; }
+        public static IO2Log log { get; set; }        
+        public static IO2Config config { get; set; }
+        
+    }
+}
